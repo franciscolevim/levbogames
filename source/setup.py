dadeca201247df0f0d9e2451e9acb706f2774cb2
Francisco Leví <franciscolevim@gmail.com>
2020-06-19 15:38:21 -0500

    Configuración del proyecto.

---
+++ source/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name = 'levbogames',
+    version = '1.0',
+    author = 'Francisco Leví',
+    author_email = 'franciscolevim@gmail.com',
+    url = 'https://github.com/franciscolevim/levbogames',
+    packages = ['levbogames', 'levbogames.environment']
+)
